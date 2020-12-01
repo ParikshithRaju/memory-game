b3fd5a3c9c86ee5c4c5af370afc360399bcb074d
Parikshith C R <rpari490@gmail.com>
2020-12-01 14:08:18 +0530

    deploy ready

---
+++ src/secretStuff.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyBbskd5ODwHgtNkzElhtDQSHdQfpn_YluM",
+    authDomain: "memory-game-aeb88.firebaseapp.com",
+    databaseURL: "https://memory-game-aeb88.firebaseio.com",
+    projectId: "memory-game-aeb88",
+    storageBucket: "memory-game-aeb88.appspot.com",
+    messagingSenderId: "480582403484",
+    appId: "1:480582403484:web:b498edbc0c34845ad53459",
+    measurementId: "G-1QCN87JF0G"
+};
